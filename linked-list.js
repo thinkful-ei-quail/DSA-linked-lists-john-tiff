1c9fa78790813499f5a4e45300535543f01c5a37
Tiff Joy <tiffanysummerford@gmail.com>
2020-09-24 12:51:30 -0400

    wrote insert functions

--- linked-list.js
+++ linked-list.js
@@ -1,9 +1,15 @@
+class _Node {
+    constructor(value, next) {
+        this.value = value;
+        this.next = next;
+    }
+}
 class LinkedList {
     constructor() {
         this.head = null;
     }
     insertFirst(item) {
-        this.head = new _Node(item, this.head);
+        this.head = new _Node(item, this.head); // left side is item and right side is same item but adding to list
     }
     insertLast(item) {
         if (this.head === null) {
@@ -16,6 +22,46 @@ class LinkedList {
             }
             tempNode.next = new _Node(item, null);
         }
+    }
+    insertBefore(newItem, targetItem) {
+        let currNode = this.head
+        let previousNode = null
+        if (!currNode) {
+           return this.head = new _Node(newItem, null)
+        }
+        while (currNode.value !== targetItem) {
+            if (currNode.next === null) {
+                return null;
+            }
+            else {
+                previousNode = currNode
+                currNode = currNode.next;
+            }
+        }
+        if (previousNode === null) {
+            return this.head = new _Node(newItem, this.head)
+        }
+        return previousNode.next = new _Node(newItem, currNode)
+    }
+
+    insertAfter(newItem, targetItem) {
+       const targetNode = find(targetItem)
+       if (targetNode) {
+           targetNode.next = new _Node(newItem, targetNode.next) // left side is the target and next describes what's after, right side is inserting new node
+        }
+    }
+    insertAt(newItem, i) {
+        let currNode = this.head
+        if (i === 0) {
+            return this.head = new _Node(newItem, null)
+        }
+        for (let j = 1; j < i ; j++) {
+            if (currNode.next === null) {
+                return currNode.next = new _Node(newItem, currNode.next)
+            }
+            currNode = currNode.next
+        }
+        return currNode.next = new _Node(newItem, currNode.next)
     }
     find(item) { 
         // Start at the head
@@ -67,6 +113,15 @@ class LinkedList {
     }
 }
 
-main = () => {
-    
+main = () => { const SLL = new LinkedList() 
+    SLL.insertFirst('Apollo')
+    SLL.insertFirst('Boomer')
+    SLL.insertFirst('Helo')
+    SLL.insertFirst('Husker')
+    SLL.insertFirst('Starbuck')
+    SLL.insertFirst('Tauhida')
+    SLL.remove('Squirrel')
+
+    return SLL
 }
+console.log(main())
